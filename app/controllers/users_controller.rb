b9ea54bb30e9e7974a810635090b5c4a68165790
rustyhutto <rhutto92@gmail.com>
2017-01-09 15:44:24 -0800

    Update users migration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,26 @@
 class UsersController < ApplicationController
-  
+
+  def show
+    @user = User.find(params[:id])
+    @checklists = Checklist.includes(:users).where(users: { id: @user.id })
+  end
+
   def new
     @user = User.new
   end
 
+  def create 
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else 
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:username, :email, :password, :location, :groups)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170105015801) do
+ActiveRecord::Schema.define(version: 20170109232219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,9 @@ ActiveRecord::Schema.define(version: 20170105015801) do
     t.date     "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
     t.index ["bird_id"], name: "index_checklists_on_bird_id", using: :btree
+    t.index ["user_id"], name: "index_checklists_on_user_id", using: :btree
   end
 
   create_table "checklists_users", force: :cascade do |t|
