d0e2362b9a632629a77969f71f6fc5cbe3c125a8
rustyhutto <rhutto92@gmail.com>
2017-01-11 16:19:31 -0800

    Add BCrypt and password helper methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  resources :users
-
-  devise_for :users
-  
-  get 'users/new'
 
   root 'users#new'
+  get 'users/new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
+  resources :users
+  devise_for :users
+  
 
 end
